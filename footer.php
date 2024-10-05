ec421092305cfe4206ac0ad90d62c72f6e245c82
Angel <angel22manalo@gmail.com>
2024-10-05 14:17:24 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+<footer>
+    <p>&copy; PAMANTASAN NG LUNGSOD NG MUNTINLUPA</p>
+    <p>Group 10 Exercise No. 6 </p>
+    <nav>
+        <ul>
+            <li><a href="terms.php">Terms & Conditions</a></li>
+            <li><a href="privacy.php">Privacy Policy</a></li>
+        </ul>
+    </nav>
+</footer>
